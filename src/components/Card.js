76cfaafbfc5e472ec80b09b2d534d64f4d31aa03
Wiltrouble <wiltrouble@gmail.com>
2021-02-27 01:10:47 -0400

    Adding styeles and animation

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,37 @@
 import React from 'react'
-import image1 from "../assets/image1.jpg";
+import PropTypes from "prop-types";
 
-function Card() {
+import './cards.css'
+
+
+function Card({title, image, text, url}) {
+
     return (
-        <div className="card">
-            <img src={image1} className=""/>
-            <div className="card-body">
-                <h1 className="card-title">Card title</h1>
-                <p className="card-text text-secondary">Lorem Ipsum is simply dummy text of the printing and typesetting 
-                    industry. Lorem Ipsum has been the industry's standard dummy 
-                    text ever since the 1500s</p>
+        <div className="card text-center bg-dark animate__animated animate__fadeInUp">
+            <div className="overflow">
+            <img src={image} className="card-img-top" alt="img"/>
+            </div>
+            <div className="card-body text-light">
+                <h1 className="card-title">{title}</h1>
+                <p className="card-text text-secondary">
+                    {
+                        text ? text : `Lorem Ipsum is simply dummy text of the printing and typesetting
+                         industry. Lorem Ipsum has been the industry's standard`
+                    }
+                </p>
+                <a href={url} className="btn btn-outline-secondary rounded-0" target="_blank">
+                    Go to {title}
+                </a>    
             </div>
         </div>
     )
+}
+
+Card.propTypes = {
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string,
+    image: PropTypes.string,
+    text: PropTypes.string
 }
 
 export default Card
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,19 +1,40 @@
 import React from 'react'
 import Card from './Card'
 
+import image1 from "../assets/image1.jpg";
+import image2 from "../assets/image2.jpg";
+import image3 from "../assets/image3.jpg";
+
+const cards = [
+    {
+        id: 1,
+        title: 'google.com',
+        image: image1,
+        url: 'https:google.com',
+    },{
+        id: 2,
+        title: 'netflix.com',
+        image: image2,
+        url: 'https:netflix.com'
+    },{
+        id: 3,
+        title: 'youtube.com',
+        image: image3,
+        url: 'https:youtube.com'
+    }
+]
+
 function Cards() {
     return (
         <div className="container d-flex justify-content-center align-items-center h-100 ">
             <div className="row">
-                <div className="col-md-4">
-                    <Card></Card>
-                </div>
-                <div className="col-md-4">
-                    <Card></Card>
-                </div>
-                <div className="col-md-4">
-                    <Card></Card>
-                </div>
+                {
+                    cards.map(card => (
+                        <div className="col-md-4" key={card.id}>
+                            <Card title={card.title} image={card.image} url={card.url} text={card.text}></Card>
+                        </div>
+                    ))
+                }
             </div>
         </div>
         
